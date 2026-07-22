098b00700855236f0d74eaaddf4345b68b0cba52
Tomasz Skierś <tomsz.skiers@gmail.com>
2026-07-22 09:17:19 +0200

    vehicleList 3 refaktoring useCallback and useMemo

--- src/components/settingsForm_1/VEHICLES_FORM_2/vehicleList_3.tsx
+++ src/components/settingsForm_1/VEHICLES_FORM_2/vehicleList_3.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
 import {
   Select,
   SelectContent,
@@ -10,16 +8,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
 import { Separator } from "@/components/ui/separator";
-
 import { PlusCircle } from "lucide-react";
-
 import { useFilteredVehicles } from "@/customHooks/useFilteredVehicles";
 import { useVehicalStorage2 } from "@/store/useVehicleStorage2";
-
 import { bodyType, Vehicle } from "../../../../constants/initialData";
-
 import AddVehicleDialog_3 from "./addVehicleDialog_3";
 import { RemoveVehicleDialog_2 } from "./removeVehicleDialog_2";
 
@@ -29,7 +22,6 @@ import { RemoveVehicleDialog_2 } from "./removeVehicleDialog_2";
 
 export default function VehicleList_3() {
   const [openAddVehicleDialog, setOpenAddVehicleDialog] = useState(false);
-
   const [vehicleToDelete, setVehicleToDelete] = useState<Vehicle | null>(null);
 
   const handleSelectDelete = useCallback((vehicle: Vehicle) => {
@@ -51,7 +43,6 @@ export default function VehicleList_3() {
       <Card className="flex-1">
         <CardHeader className="flex justify-between items-center">
           <SelectKindOfVehicle />
-
           <BtnAddVehicle showDialog={handleOpenAddVehicle} />
         </CardHeader>
 
@@ -94,33 +85,12 @@ const VehicleListWrapper = React.memo(function VehicleListWrapper({
   const vehicles = useFilteredVehicles();
 
   return (
-    <div
-      className="
-        absolute
-        inset-0
-        flex
-        flex-col
-        gap-3
-        overflow-auto
-      "
-    >
+    <div className="absolute inset-0 flex flex-col gap-3 overflow-auto">
       {vehicles.length === 0 ? (
-        <div
-          className="
-            mx-auto
-            text-2xl
-            font-extrabold
-            text-center
-          "
-        >
+        <div className="mx-auto text-2xl font-extrabold text-center">
           brak danych o pojazdach
           <br />
-          <span
-            className="
-              text-sm
-              text-muted-foreground
-            "
-          >
+          <span className="text-sm text-muted-foreground">
             kliknij dodaj pojazd
           </span>
         </div>
@@ -139,16 +109,14 @@ VehicleListWrapper.displayName = "VehicleListWrapper";
 // =====================================================
 // MAP VEHICLES
 // =====================================================
-
+//* nie muszę przekazywać Vehicles w propsach bo mam zustanad 
 interface VehiclesListMapWrapperProps {
   vehicles: Vehicle[];
-
   onSelectDelete: (vehicle: Vehicle) => void;
 }
 
 const VehiclesListMapWrapper = React.memo(function VehiclesListMapWrapper({
   vehicles,
-
   onSelectDelete,
 }: VehiclesListMapWrapperProps) {
   return (
@@ -172,83 +140,40 @@ VehiclesListMapWrapper.displayName = "VehiclesListMapWrapper";
 
 interface VehicleMapRowProps {
   vehicle: Vehicle;
-
   onSelectDelete: (vehicle: Vehicle) => void;
 }
 
 export const VehicleMapRow = React.memo(function VehicleMapRow({
   vehicle,
-
   onSelectDelete,
 }: VehicleMapRowProps) {
   return (
-    <Card
-      className="
-    grid
-    grid-cols-[1fr]
-    md:grid-cols-[1fr_auto]
-    mx-5
-    bg-background
-  "
-    >
-      <div
-        className="
-  flex-1
-  flex
-  flex-col
-  justify-center
- "
-      >
+    <Card className="grid grid-cols-[1fr] md:grid-cols-[1fr_auto] mx-5 bg-background">
+      <div className="flex-1 flex flex-col justify-center">
         <CardHeader>
           <span className="text-muted-foreground">marka pojazdu:</span>
-
           <span className="font-extrabold">{vehicle.name}</span>
         </CardHeader>
 
         <CardContent>
-          <div
-            className="
- flex
- items-center
- gap-2
- "
-          >
+          <div className="flex items-center gap-2">
             <span className="text-muted-foreground">typ pojazdu:</span>
-
             {vehicle.types.map((type, index) => (
               <span key={index}>{type}</span>
             ))}
           </div>
 
           <span className="text-muted-foreground">kategoria pojazdu:</span>
-
           <span>{vehicle.category}</span>
-
           <br />
-
-          <span
-            className="
-text-xs
-text-muted-foreground
-"
-          >
+          <span className="text-xs text-muted-foreground">
             id: {vehicle.id}
           </span>
         </CardContent>
       </div>
 
-      <div
-        className="
-flex
-flex-col
-justify-center
-gap-5
-p-4
-md:flex-row
-"
-      >
+      <div className="flex flex-col justify-center gap-5 p-4 md:flex-row">
         <Button size="sm">edytuj</Button>
-
         <Button
           size="sm"
           variant="destructive"
@@ -271,13 +196,11 @@ function SelectKindOfVehicle() {
   const kindOfVehicle = useVehicalStorage2((state) => state.activeSort);
   const setKindOfVehicle = useVehicalStorage2((state) => state.setActiveSort);
 
-  // Pobieramy przefiltrowaną listę i odczytujemy tylko jej długość
   const filteredVehicles = useFilteredVehicles();
   const count = filteredVehicles.length;
 
   return (
     <div className="flex flex-col gap-3">
-      {/* Wyświetlamy aktualny licznik */}
       <CardTitle>Lista pojazdów: {count}</CardTitle>
 
       <Select value={kindOfVehicle} onValueChange={setKindOfVehicle}>
@@ -311,7 +234,6 @@ const BtnAddVehicle = React.memo(function BtnAddVehicle({
   return (
     <Button onClick={showDialog}>
       <PlusCircle className="text-chart-1" />
-
       <span>dodaj pojazd</span>
     </Button>
   );
